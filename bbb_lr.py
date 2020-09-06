4ecde2512d9a4db1f963267d70fa27be57d78213
Leo Nguyen <leo.nguyen@fortemedia.com>
2020-09-07 02:01:00 +0900

    - change to initialize param when creating model

--- bbb_lr.py
+++ bbb_lr.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import numpy as np
 import config as cf
 import dataset as ds
 import Utils
@@ -14,20 +15,60 @@ from BBBLearning.lr import lr
 cost_function="cross_entropy"
 optimizer="GradientDecent"
 initializer="random"
+model_output_1="./MODEL/bbb_lr_1.npy"
+model_output_2="./MODEL/bbb_lr_2.npy"
 
-# Traning
+# Parameter
 MINIBATCH_SIZE = 10000
-learning_rate = 0.1
-iteration = 1000
+learning_rate = 0.2
+iteration = 10000
 beta1 = 0.0
 beta2 = 0.0
 epsilon = 0.0
 print_cost = True
 
+# Training
+# Mixed dataset
+'''
 model = lr.model(cf.NUM_OF_FEATURES, cost_function, optimizer, initializer)
 batch = ds.load_dataset(cf.out_mixed_dataset, 0) # load dataset batch position 0
-params, costs = lr.train(model, batch['X'], batch['y'], iteration, learning_rate, beta1, beta2, epsilon, "./MODEL/bbb_lr_1.npy", print_cost)
+params, costs = lr.train(model, batch['X'], batch['y'], iteration, learning_rate, beta1, beta2, epsilon, model_output_1, print_cost)
+'''
 
-#batch = ds.load_dataset(cf.out_mixed_dataset, 1) # load dataset batch position 0
-#model = lr.load_model("./MODEL/bbb_lr_1.npy")
-#params, costs = lr.retrain(model, batch['X'], batch['y'], iteration, learning_rate, beta1, beta2, epsilon, "./MODEL/bbb_lr_1.npy", print_cost)
+'''
+batch = ds.load_dataset(cf.out_mixed_dataset, 0)
+model = lr.load_model(model_output_1)
+params, costs = lr.train(model, batch['X'], batch['y'], iteration, learning_rate, beta1, beta2, epsilon, model_output_1, print_cost)
+'''
+
+#masked dataset
+'''
+batch = ds.load_dataset(cf.out_masked_dataset, 0)
+model = lr.load_model(model_output_1)
+params, costs = lr.train(model, batch['X'], batch['y'], iteration, learning_rate, beta1, beta2, epsilon, model_output_1, print_cost)
+'''
+
+# Predict
+#batch = ds.load_dataset(cf.out_mixed_dataset, 0)
+batch = ds.load_dataset(cf.out_masked_dataset, 0)
+model = lr.load_model(model_output_1)
+params = {}
+params['w'] = model['w']
+params['b'] = model['b']
+Y_pred = lr.predict(params, batch['X'])
+Y =  batch['y']
+paths = batch['paths']
+np.set_printoptions(threshold=sys.maxsize)
+
+a = paths[((Y==1)&(Y_pred==1))[0]]
+b = paths[((Y==1)&(Y_pred==0))[0]]
+c = paths[((Y==0)&(Y_pred==0))[0]]
+d = paths[((Y==0)&(Y_pred==1))[0]]
+
+accuracy = (len(a) + len(c))/Y.shape[1]
+precision = len(a)/(len(a)+len(d))
+recall = len(a)/(len(a)+len(b))
+
+print('accuracy = ' + str(accuracy))
+print('precision = ' + str(precision))
+print('recall = ' + str(recall))
