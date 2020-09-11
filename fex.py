f5b7965dff58c17a886dd6f2bbf0de4574c94bc9
Leo Nguyen <leo.nguyen@fortemedia.com>
2020-09-11 15:08:05 +0900

    - change normalization method

--- fex.py
+++ fex.py
@@ -8,7 +8,8 @@ def fex(filepath):
     image = cv2.imread(filepath, 0)
     if image is not None:
         resize_ = cv2.resize(image, (cf.WIDTH,cf.HEIGHT), interpolation=cv2.INTER_CUBIC)
-        norm_ = cv2.normalize(resize_, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        #norm_ = cv2.normalize(resize_, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        norm_ = resize_/255.0 - 0.5
         x = np.reshape(norm_,(norm_.shape[0]*norm_.shape[1], 1))        
     return x
 
