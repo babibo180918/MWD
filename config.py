2888d2a7d75385b6ca44f427027fa8de70c1c9e3
Leo Nguyen <leo.nguyen@fortemedia.com>
2020-09-11 15:18:58 +0900

    - add to make balanced dataset

--- config.py
+++ config.py
@@ -12,13 +12,12 @@ IN_PATH = os.path.abspath(os.path.join('..','DATA/RMFD'))
 IN_MASKED_FACE_PATH = os.path.join(IN_PATH, 'AFDB_masked_face_dataset')
 IN_MASKED_FACE_AUGMENTED_PATH = os.path.join(IN_PATH, 'AFDB_masked_face_dataset_augmented')
 IN_FACE_PATH = os.path.join(IN_PATH, 'AFDB_face_dataset')
-#test cases
-IN_TESTCASE1 = os.path.abspath(os.path.join('..','DATA/TESTCASE1'))
 
 #OUTPUT DATA
 OUT_PATH = './DATASET/'
-out_testcase1 = OUT_PATH + 'testcase1.npy'
 out_masked_dataset = OUT_PATH + 'masked_dataset.npy'
 out_face_dataset = OUT_PATH + 'face_dataset.npy'
 out_mixed_dataset = OUT_PATH + 'mixed_dataset.npy'
 out_mixed_balanced_dataset = OUT_PATH + 'mixed_balanced_dataset.npy' # 53639 samples with balanced distribution.
+out_mixed_balanced_dataset_train = OUT_PATH + 'mixed_balanced_dataset_train.npy' # 50000 samples with balanced distribution.
+out_mixed_balanced_dataset_test = OUT_PATH + 'mixed_balanced_dataset_test.npy' # 3639 samples with balanced distribution.
--- test.py
+++ test.py
@@ -10,6 +10,7 @@ import Utils
 
 #face dataset
 #ds.create_dataset(cf.IN_FACE_PATH, cf.out_face_dataset, cf.NUM_OF_CLASSES, 0, 1000)
+
 '''
 #mixed dataset
 masked_filepaths = Utils.getFileList(cf.IN_MASKED_FACE_PATH)
@@ -22,10 +23,18 @@ ds.create_mixed_dataset(cf.out_mixed_dataset, cf.NUM_OF_CLASSES, masked_filepath
 # mixed balanced dataset
 masked_filepaths = Utils.getFileList(cf.IN_MASKED_FACE_AUGMENTED_PATH)
 masked_labels = [1]*len(masked_filepaths)
+# suffle random masked images
+sample_idxs = random.sample(range(0,len(masked_filepaths)), len(masked_filepaths))
+masked_filepaths = [masked_filepaths[i] for i in sample_idxs]
+masked_labels = [masked_labels[i] for i in sample_idxs]
+
 face_filepaths = Utils.getFileList(cf.IN_FACE_PATH)
 face_labels = [0]*len(face_filepaths)
 # get 30000 samples of faces
-sample_idxs = random.sample(range(0,len(face_filepaths)), 30000)         
+sample_idxs = random.sample(range(0,len(face_filepaths)), 30000)       
 face_filepaths = [face_filepaths[i] for i in sample_idxs]
 face_labels = [face_labels[i] for i in sample_idxs]
-ds.create_mixed_dataset(cf.out_mixed_balanced_dataset, cf.NUM_OF_CLASSES, masked_filepaths, masked_labels, face_filepaths, face_labels)
+# Create training dataset
+ds.create_mixed_dataset(cf.out_mixed_balanced_dataset_train, cf.NUM_OF_CLASSES, masked_filepaths[0:22000], masked_labels[0:22000], face_filepaths[0:28000], face_labels[0:28000])
+# Create test dataset
+ds.create_mixed_dataset(cf.out_mixed_balanced_dataset_test, cf.NUM_OF_CLASSES, masked_filepaths[22000:len(masked_filepaths)], masked_labels[22000:len(masked_labels)], face_filepaths[28000:len(face_filepaths)], face_labels[28000:len(face_labels)])
