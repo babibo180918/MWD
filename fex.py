- update normalization method to fix [Nan] cost value and train again for balanced mixed dataset

fex.py
@@ -9,7 +9,7 @@ def fex(filepath):
     if image is not None:
         resize_ = cv2.resize(image, (cf.WIDTH,cf.HEIGHT), interpolation=cv2.INTER_CUBIC)
         #norm_ = cv2.normalize(resize_, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-        norm_ = resize_/255.0 - 0.5
+        norm_ = np.divide(resize_, 255.0, dtype=np.float32) - 0.5
         x = np.reshape(norm_,(norm_.shape[0]*norm_.shape[1], 1))        
     return x
 