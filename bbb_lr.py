f38967330e29159d1e6e514eafa3391076129300
Leo Nguyen <leo.nguyen@fortemedia.com>
2020-09-18 00:05:25 +0900

    - change minibatch size to 100

--- bbb_lr.py
+++ bbb_lr.py
@@ -19,7 +19,8 @@ model_output_1="./MODEL/bbb_lr_1.npy"
 model_output_2="./MODEL/bbb_lr_2.npy"
 balanced_model_output_1="./MODEL/bbb_lr_balanced_1.npy"
 balanced_model_output_2="./MODEL/bbb_lr_balanced_2.npy"
-model_path = balanced_model_output_2
+balanced_model_output_3="./MODEL/bbb_lr_balanced_3.npy"
+model_path = balanced_model_output_3
 
 # Dataset
 #train_dataset_path = cf.out_masked_dataset
@@ -32,7 +33,7 @@ train_dataset_path = cf.out_mixed_balanced_dataset_train
 test_dataset_path = cf.out_mixed_balanced_dataset_test
 
 # Parameters
-MINIBATCH_SIZE = 1000
+MINIBATCH_SIZE = 100
 learning_rate = 0.1
 iteration = 10000
 lambd = 0.0
