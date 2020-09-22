cd442af742a829ad13d728b7a8507b9a83a52ea7
Leo Nguyen <leo.nguyen@fortemedia.com>
2020-09-23 01:24:40 +0900

    - update bbb logistic regression training with new model structure and looping through num_epochs

--- bbb_lr.py
+++ bbb_lr.py
@@ -13,14 +13,15 @@ from BBBLearning.lr import lr
 
 # Model
 cost_function="cross_entropy"
-optimizer="GradientDecent"
-initializer="random"
+optimizer="GradientDescent"
+initializer="zero"
 model_output_1="./MODEL/bbb_lr_1.npy"
 model_output_2="./MODEL/bbb_lr_2.npy"
 balanced_model_output_1="./MODEL/bbb_lr_balanced_1.npy"
 balanced_model_output_2="./MODEL/bbb_lr_balanced_2.npy"
 balanced_model_output_3="./MODEL/bbb_lr_balanced_3.npy"
-model_path = balanced_model_output_3
+zero_init_model_output="./MODEL/bbb_lr_zero_init.npy"
+model_path = zero_init_model_output
 
 # Dataset
 #train_dataset_path = cf.out_masked_dataset
@@ -35,7 +36,7 @@ test_dataset_path = cf.out_mixed_balanced_dataset_test
 # Parameters
 MINIBATCH_SIZE = 100
 learning_rate = 0.1
-iteration = 10000
+num_epochs = 10000
 lambd = 0.0
 beta1 = 0.0
 beta2 = 0.0
@@ -43,19 +44,17 @@ epsilon = 0.0
 print_cost = True
 
 # Training
-#model = lr.model(cf.NUM_OF_FEATURES, cost_function, optimizer, initializer)
+model = lr.model(cf.NUM_OF_FEATURES, cost_function, optimizer, initializer)
 #model = lr.load_model(model_path)
-#batch = ds.load_dataset(train_dataset_path, 0)
-#params, costs = lr.train(model, batch['X'], batch['y'], MINIBATCH_SIZE, iteration, lambd, learning_rate, beta1, beta2, epsilon, model_path, print_cost)
-#params, costs = lr.train2(model, train_dataset_path, MINIBATCH_SIZE, iteration, lambd, learning_rate, beta1, beta2, epsilon, model_path, print_cost)
+batch = ds.load_dataset(train_dataset_path, 0)
+params, costs = lr.train(model, batch['X'], batch['y'], MINIBATCH_SIZE, num_epochs, lambd, learning_rate, beta1, beta2, epsilon, model_path, print_cost)
+#params, costs = lr.train2(model, train_dataset_path, MINIBATCH_SIZE, num_epochs, lambd, learning_rate, beta1, beta2, epsilon, model_path, print_cost)
 
 
 # Predict
 batch = ds.load_dataset(test_dataset_path, 0)
 model = lr.load_model(model_path)
-params = {}
-params['w'] = model['w']
-params['b'] = model['b']
+params = model['parameters']
 Y_pred = lr.predict(params, batch['X'])
 Y =  batch['y']
 paths = batch['paths']
