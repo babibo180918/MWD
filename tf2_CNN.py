1692bf6004885d1a461add17f004d1666abffdf7
Nhan Nguyen <nguyendtnhan@gmail.com>
2020-12-27 02:35:42 +0900

    - update data generator

--- tf2_CNN.py
+++ tf2_CNN.py
@@ -10,12 +10,14 @@ import Utils
 import tensorflow as tf
 import cv2
 import pydot
+from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
 from tensorflow.keras import initializers
 from tensorflow.keras import regularizers
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Model
+from tensorflow.keras.models import model_from_json
 from tensorflow.keras.metrics import *
 
 # Model
@@ -28,19 +30,37 @@ mwd_cnn_weights_output="./MODEL/MWD_CNN.h5"
 model_path = mwd_cnn_model_output
 weights_path = mwd_cnn_weights_output
 
-# Dataset
-train_dataset_path = cf.out_mixed_balanced_dataset_train
-test_dataset_path = cf.out_mixed_balanced_dataset_test
-
 # Parameters
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 learning_rate = 0.01
-num_epochs = 100
+num_epochs = 5
 L2_lambd = 0.0
 beta1 = 0.0
 beta2 = 0.0
 epsilon = 0.0
 print_cost = True
+
+def create_dataset(out_path, *args):
+    filepaths = []
+    labels = []    
+    numOfArgs = len(args)
+    
+    if numOfArgs%2 != 0:
+        print("Number of arguments are incorrect.")
+        return
+
+    for i in range(0, numOfArgs, 2):
+        filepaths.extend(args[i])
+        labels.extend(args[i+1])
+
+    numOfDataset = numOfArgs//2
+    sumOfSamples = len(labels)
+    sample_idxs = random.sample(range(0,sumOfSamples), sumOfSamples)         
+    filepaths = [filepaths[i] for i in sample_idxs]
+    labels = [labels[i] for i in sample_idxs]
+    inputs = {'filepaths':filepaths, 'labels':labels}
+    np.save(out_path, inputs)
+    return (filepaths, labels)
 
 def dataset_steps(filepaths, bs):
     steps = len(filepaths) // bs
@@ -51,21 +71,17 @@ def data_generator(filepaths, labels, bs):
     print("data size: " + str(datasize))
     if bs > datasize:
         bs = datasize
+    numOfBatch = datasize//bs
+    print("numOfBatch: " + str(numOfBatch))
     batch_X = np.zeros((bs, cf.WIDTH, cf.HEIGHT, 3))
     batch_Y = np.zeros((bs, 1))
     while True:
         i = 0
-        for f in range(0,datasize):
-            image = cv2.imread(filepaths[f])
-            if image is not None:
-                resize_ = cv2.resize(image, (cf.WIDTH,cf.HEIGHT), interpolation=cv2.INTER_CUBIC)
-                #norm_ = cv2.normalize(resize_, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-                batch_X[i,:,:,:] = np.divide(resize_, 255.0, dtype=np.float32) - 0.5
-                batch_Y[i,0] = labels[f]
-                i = i + 1
-                if i == bs:
-                    i = 0
-                    yield(batch_X, batch_Y)
+        for i in range(0,numOfBatch):
+            filepaths_batch = filepaths[i*bs:(i+1)*bs]
+            batch_X = np.array([cv2.resize(cv2.imread(file_name), (cf.WIDTH, cf.HEIGHT)) for file_name in filepaths_batch])/255.0 - 0.5
+            batch_Y = np.array(labels[i*bs:(i+1)*bs])
+            yield(batch_X, batch_Y)
 
 def data_generator2(dataset, bs):
     p = Path(dataset)
@@ -121,49 +137,73 @@ def predict(model, filepaths, labels):
     datasize = len(filepaths)
     X = []
     for f in range(0,datasize):
-        image = cv2.imread(f)
+        image = cv2.imread(filepaths[f])
         if image is not None:
             resize_ = cv2.resize(image, (cf.WIDTH,cf.HEIGHT), interpolation=cv2.INTER_CUBIC)
             norm_ = np.divide(resize_, 255.0, dtype=np.float32) - 0.5
-            X.appends(norm_)
-    preds = model.evaluate(x=X, y=labels)
+            X.append(norm_)
+    preds = model.evaluate(x=np.array(X), y=np.array(labels))
     print ("Loss = " + str(preds[0]))
     print ("Accuracy = " + str(preds[1]))
     print ("Precision = " + str(preds[2]))
     print ("Recall = " + str(preds[3]))
 
+#############################
+#   Image pre-processing    #
+#############################
+
 # filtering out the non-image files
-#Utils.image_filter(cf.IN_MASKED_FACE_PATH)
+#Utils.image_filter(cf.IN_MASKED_FACE_AUGMENTED_PATH)
 #Utils.image_filter(cf.IN_FACE_PATH)
 
-# mixed balanced dataset
-masked_filepaths = Utils.getFileList(cf.IN_MASKED_FACE_PATH)
+# dataset augmentation
+#Utils.image_augmentation(cf.IN_MASKED_FACE_AUGMENTED_PATH)
+
+'''
+# create mixed balanced dataset
+masked_filepaths = Utils.getFileList(cf.IN_MASKED_FACE_AUGMENTED_PATH)
 masked_labels = [1]*len(masked_filepaths) 
 # suffle random masked images
 sample_idxs = random.sample(range(0,len(masked_filepaths)), len(masked_filepaths))
 masked_filepaths = [masked_filepaths[i] for i in sample_idxs]
 masked_labels = [masked_labels[i] for i in sample_idxs]
-
 face_filepaths = Utils.getFileList(cf.IN_FACE_PATH)
 face_labels = [0]*len(face_filepaths)
 # get 30000 samples of faces
 sample_idxs = random.sample(range(0,len(face_filepaths)), 30000)       
 face_filepaths = [face_filepaths[i] for i in sample_idxs]
 face_labels = [face_labels[i] for i in sample_idxs]
+filepaths, labels = create_dataset(cf.out_mixed_balanced_dataset, masked_filepaths, masked_labels, face_filepaths, face_labels)
 
-filepaths = []
-labels = []
-filepaths.extend(masked_filepaths)
-filepaths.extend(face_filepaths)
-labels.extend(masked_labels)
-labels.extend(face_labels)
-sumOfSamples = len(labels)
-sample_idxs = random.sample(range(0,sumOfSamples), sumOfSamples)         
-filepaths = [filepaths[i] for i in sample_idxs]
-labels = [labels[i] for i in sample_idxs]
+# load dataset
+inputs = np.load(cf.out_mixed_balanced_dataset, allow_pickle=True)
+filepaths = inputs.item().get('filepaths')
+labels = inputs.item().get('labels')
+filepaths_train, filepaths_val, labels_train, labels_val = train_test_split(filepaths, labels, test_size=0.2, random_state=1)
+
 
 # Model
 model = MWD_CNN((cf.WIDTH, cf.HEIGHT, 3),cf.NUM_OF_CLASSES, initializer)
-train(model, filepaths[0:30080], labels[0:30080], BATCH_SIZE, num_epochs, learning_rate, beta1, beta2, epsilon, model_path, print_cost)
-predict(model, filepaths[30080:len(filepaths)], labels[30080:len(labels)])
+train(model, filepaths_train, labels_train, BATCH_SIZE, num_epochs, learning_rate, beta1, beta2, epsilon, model_path, print_cost)
+model.summary()
+# serialize model to JSON
+model_json = model.to_json()
+with open(mwd_cnn_model_output, "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights(mwd_cnn_weights_output)
+print("Saved model to disk")
+'''
 
+inputs = np.load(cf.out_mixed_balanced_dataset, allow_pickle=True)
+filepaths = inputs.item().get('filepaths')
+labels = inputs.item().get('labels')
+filepaths_train, filepaths_val, labels_train, labels_val = train_test_split(filepaths, labels, test_size=0.2, random_state=1)
+
+json_file = open(mwd_cnn_model_output, 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.compile(optimizer=optimizers.SGD(learning_rate = learning_rate), loss=cost_function, metrics=['accuracy',Precision(), Recall()])
+model.load_weights(mwd_cnn_weights_output)
+predict(model, filepaths_val, labels_val)
